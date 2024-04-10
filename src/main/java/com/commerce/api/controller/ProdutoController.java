fc3763737daf57c968f473c1d037f38f1d32cf71
cristian-95 <cristian_santos9@outlook.com>
2024-04-10 15:53:17 -0300

    documentação: Spring Doc e README

--- src/main/java/com/commerce/api/controller/ProdutoController.java
+++ src/main/java/com/commerce/api/controller/ProdutoController.java
@@ -14,12 +14,19 @@ import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.media.Content;
 import com.commerce.api.exception.ResourceNotFoundException;
 import com.commerce.api.model.Produto;
 import com.commerce.api.model.dto.ProdutoDTO;
 import com.commerce.api.service.ProdutoService;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import io.swagger.v3.oas.annotations.media.ArraySchema;
+import io.swagger.v3.oas.annotations.media.Schema;
+
+@Tag(name = "Produtos", description = "Gerenciamento de produtos.")
 @RestController
 @RequestMapping("/produtos")
 public class ProdutoController {
@@ -28,30 +35,95 @@ public class ProdutoController {
     private ProdutoService service;
 
     @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
-    public ResponseEntity<List<Produto>> getAll(){
-        return ResponseEntity.ok(service.getAllProdutos());        
+    @Operation(summary = "Lista todos os produtos", description = "Consulta o banco de dados e retorna todos os produtos.", tags = {
+            "Produtos" }, responses = {
+                    @ApiResponse(description = "Success", responseCode = "200", 
+                    content = @Content(
+                        mediaType = "application/json",
+                        array = @ArraySchema(schema = @Schema(implementation = Produto.class))
+                    )),
+                    @ApiResponse(description = "Bad Request", responseCode = "400", content = @Content),
+                    @ApiResponse(description = "Unauthorized", responseCode = "401", content = @Content),
+                    @ApiResponse(description = "Forbidden", responseCode = "403", content = @Content),
+                    @ApiResponse(description = "Not Found", responseCode = "404", content = @Content),
+                    @ApiResponse(description = "Internal Server Error", responseCode = "500", content = @Content)
+            })
+    public ResponseEntity<List<Produto>> getAll() {
+        return ResponseEntity.ok(service.getAllProdutos());
     }
 
-    @GetMapping(value = "/{id}", produces =  MediaType.APPLICATION_JSON_VALUE)
-    public ResponseEntity<?> getById(@PathVariable("id")Long id) throws ResourceNotFoundException{
-        Produto  produto = service.getProdutoById(id);
-        if (produto == null) return new ResponseEntity<>("Produto não encontrado.", HttpStatus.NOT_FOUND);
+    @GetMapping(value = "/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
+    @Operation(summary = "Exibe um produto", description = "Consulta o banco de dados e retorna um determinado produto, a partir do número de id passado na URI.", tags = {
+        "Produtos" }, responses = {
+                @ApiResponse(description = "Success", responseCode = "200", 
+                content = @Content(
+                    mediaType = "application/json",
+                    array = @ArraySchema(schema = @Schema(implementation = Produto.class))
+                )),
+                @ApiResponse(description = "Bad Request", responseCode = "400", content = @Content),
+                @ApiResponse(description = "Unauthorized", responseCode = "401", content = @Content),
+                @ApiResponse(description = "Forbidden", responseCode = "403", content = @Content),
+                @ApiResponse(description = "Not Found", responseCode = "404", content = @Content),
+                @ApiResponse(description = "Internal Server Error", responseCode = "500", content = @Content)
+        })
+    public ResponseEntity<?> getById(@PathVariable("id") Long id) throws ResourceNotFoundException {
+        Produto produto = service.getProdutoById(id);
+        if (produto == null)
+            return new ResponseEntity<>("Produto não encontrado.", HttpStatus.NOT_FOUND);
         return ResponseEntity.ok(produto);
     }
 
     @PostMapping(produces = MediaType.APPLICATION_JSON_VALUE, consumes = MediaType.APPLICATION_JSON_VALUE)
-    public ResponseEntity<Produto> create(@RequestBody ProdutoDTO dto){
+    @Operation(summary = "Registra um produto", description = "Instancia um novo objeto do tipo Produto e salva no banco de dados, a propriedade loja permanece nula inicialmente.", tags = {
+        "Produtos" }, responses = {
+                @ApiResponse(description = "Created", responseCode = "201", 
+                content = @Content(
+                    mediaType = "application/json",
+                    array = @ArraySchema(schema = @Schema(implementation = Produto.class))
+                )),
+                @ApiResponse(description = "Bad Request", responseCode = "400", content = @Content),
+                @ApiResponse(description = "Unauthorized", responseCode = "401", content = @Content),
+                @ApiResponse(description = "Forbidden", responseCode = "403", content = @Content),                
+                @ApiResponse(description = "Internal Server Error", responseCode = "500", content = @Content)
+        })
+    public ResponseEntity<Produto> create(@RequestBody ProdutoDTO dto) {
         return new ResponseEntity<Produto>(service.createProduto(dto), HttpStatus.CREATED);
     }
 
     @PutMapping(produces = MediaType.APPLICATION_JSON_VALUE, consumes = MediaType.APPLICATION_JSON_VALUE)
-    public ResponseEntity<Produto> update(@RequestBody ProdutoDTO dto) throws ResourceNotFoundException{
+    @Operation(summary = "Atualiza um produto", description = "Instancia um novo objeto do tipo Produto, sobrescreve um produto existente com a mesma id e salva no banco de dados.", tags = {
+        "Produtos" }, responses = {
+                @ApiResponse(description = "Success", responseCode = "200", 
+                content = @Content(
+                    mediaType = "application/json",
+                    array = @ArraySchema(schema = @Schema(implementation = Produto.class))
+                )),
+                @ApiResponse(description = "Bad Request", responseCode = "400", content = @Content),
+                @ApiResponse(description = "Unauthorized", responseCode = "401", content = @Content),
+                @ApiResponse(description = "Forbidden", responseCode = "403", content = @Content),   
+                @ApiResponse(description = "Not Found", responseCode = "404", content = @Content),             
+                @ApiResponse(description = "Internal Server Error", responseCode = "500", content = @Content)
+        })
+    public ResponseEntity<Produto> update(@RequestBody ProdutoDTO dto) throws ResourceNotFoundException {
         return ResponseEntity.ok(service.updateProduto(dto));
     }
 
     @DeleteMapping(value = "/{id}")
-    public ResponseEntity<?> delete(@PathVariable("id")Long id) throws Exception{
+    @Operation(summary = "Remove um produto", description = "Consulta o banco de dados e remove um determinado produto do banco de dados, a partir do número de id passado na URI.", tags = {
+        "Produtos" }, responses = {
+                @ApiResponse(description = "No Content", responseCode = "204", 
+                content = @Content(
+                    mediaType = "application/json",
+                    array = @ArraySchema(schema = @Schema(implementation = Produto.class))
+                )),
+                @ApiResponse(description = "Bad Request", responseCode = "400", content = @Content),
+                @ApiResponse(description = "Unauthorized", responseCode = "401", content = @Content),
+                @ApiResponse(description = "Forbidden", responseCode = "403", content = @Content),   
+                @ApiResponse(description = "Not Found", responseCode = "404", content = @Content),             
+                @ApiResponse(description = "Internal Server Error", responseCode = "500", content = @Content)
+        })
+    public ResponseEntity<?> delete(@PathVariable("id") Long id) throws Exception {
         service.deleteProduto(id);
-        return ResponseEntity.noContent().build();        
+        return ResponseEntity.noContent().build();
     }
 }
